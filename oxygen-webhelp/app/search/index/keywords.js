0a209812bbc9444d7857bd0b1bb9ebff3eb3e73b
Camil0086 <kamil.nowak.0086@gmail.com>
2021-05-19 23:36:43 +0200

    svg_and_styleguide_update

--- oxygen-webhelp/app/search/index/keywords.js
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -1,4 +1,4 @@
-define(function() {var keywords=[{w:"Our",p:["p0","p27"]},{w:"friend",p:["p0"]},{w:"the",p:["p0","p4","p19","p20","p21"]},{w:"tree",p:["p0"]},{w:"Solid",p:["p1","p3","p26"]},{w:"wood",p:["p1","p3","p16","p18","p23"]},{w:"On",p:["p2"]},{w:"trips",p:["p2"]},{w:"to",p:["p2","p21","p27"]},{w:"India",p:["p2"]},{w:"furniture",p:["p3","p21","p26"]},{w:"stand",p:["p3"]},{w:"for",p:["p3","p26"]},{w:"environmental",p:["p3"]},{w:"friendliness",p:["p3"]},{w:"and",p:["p3","p26"]},{w:"sustainability",p:["p3"]},{w:"acacia",p:["p4"]},{w:"-",p:["p4","p22","p27"]},{w:"a",p:["p4","p18","p19"]},{w:"real",p:["p4"]},{w:"multi-talent",p:["p4"]},{w:"Beam",p:["p5"]},{w:"oak",p:["p5"]},{w:"vs.",p:["p5"]},{w:"wild",p:["p5"]},{w:"What",p:["p6"]},{w:"is",p:["p6"]},{w:"Sheesham",p:["p6"]},{w:"called",p:["p6"]},{w:"also",p:["p6"]},{w:"Rosewood",p:["p6"]},{w:"hard",p:["p7"]},{w:"wax",p:["p7"]},{w:"plane",p:["p7"]},{w:"tannins",p:["p8"]},{w:"mimosa",p:["p9"]},{w:"bark",p:["p9"]},{w:"sapwood",p:["p10"]},{w:"heartwood",p:["p11"]},{w:"bast",p:["p12"]},{w:"resin",p:["p13"]},{w:"nucleation",p:["p14"]},{w:"crown",p:["p15"]},{w:"roots",p:["p15"]},{w:"stain",p:["p16"]},{w:"Glossary",p:["p17"]},{w:"Known",p:["p18"]},{w:"types",p:["p18"]},{w:"of",p:["p18","p20","p21"]},{w:"at",p:["p18","p19"]},{w:"glance",p:["p18","p19"]},{w:"most",p:["p19"]},{w:"popular",p:["p19"]},{w:"furnishing",p:["p19"]},{w:"styles",p:["p19"]},{w:"symbolism",p:["p20"]},{w:"trees",p:["p20"]},{w:"From",p:["p21"]},{w:"website",p:["p21"]},{w:"living",p:["p21"]},{w:"room,",p:["p21"]},{w:"how",p:["p21","p22","p27"]},{w:"does",p:["p21","p22"]},{w:"my",p:["p21"]},{w:"favorite",p:["p21"]},{w:"piece",p:["p21"]},{w:"get",p:["p21"]},{w:"home?",p:["p21"]},{w:"Complaints",p:["p22"]},{w:"it",p:["p22"]},{w:"work?",p:["p22"]},{w:"Removing",p:["p23","p24","p25"]},{w:"worms",p:["p23"]},{w:"scratches",p:["p24"]},{w:"stains",p:["p25"]},{w:"First",p:["p26"]},{w:"aid",p:["p26"]},{w:"tips",p:["p26"]},{w:"tricks",p:["p26"]},{w:"Order",p:["p27"]},{w:"procedure",p:["p27"]},{w:"buy",p:["p27"]},{w:"in",p:["p27"]},{w:"online",p:["p27"]},{w:"shop",p:["p27"]}];
+define(function() {var keywords=[{w:"Our",p:["p0","p28"]},{w:"friend",p:["p0"]},{w:"the",p:["p0","p4","p20","p21","p22"]},{w:"tree",p:["p0"]},{w:"Solid",p:["p1","p3","p27"]},{w:"wood",p:["p1","p3","p16","p19","p24"]},{w:"On",p:["p2"]},{w:"trips",p:["p2"]},{w:"to",p:["p2","p22","p28"]},{w:"India",p:["p2"]},{w:"furniture",p:["p3","p22","p27"]},{w:"stand",p:["p3"]},{w:"for",p:["p3","p27"]},{w:"environmental",p:["p3"]},{w:"friendliness",p:["p3"]},{w:"and",p:["p3","p27"]},{w:"sustainability",p:["p3"]},{w:"acacia",p:["p4"]},{w:"-",p:["p4","p23","p28"]},{w:"a",p:["p4","p19","p20"]},{w:"real",p:["p4"]},{w:"multi-talent",p:["p4"]},{w:"Beam",p:["p5"]},{w:"oak",p:["p5"]},{w:"vs.",p:["p5"]},{w:"wild",p:["p5"]},{w:"What",p:["p6"]},{w:"is",p:["p6"]},{w:"Sheesham",p:["p6"]},{w:"called",p:["p6"]},{w:"also",p:["p6"]},{w:"Rosewood",p:["p6"]},{w:"hard",p:["p7"]},{w:"wax",p:["p7"]},{w:"plane",p:["p7"]},{w:"tannins",p:["p8"]},{w:"mimosa",p:["p9"]},{w:"bark",p:["p9"]},{w:"sapwood",p:["p10"]},{w:"heartwood",p:["p11"]},{w:"bast",p:["p12"]},{w:"resin",p:["p13"]},{w:"nucleation",p:["p14"]},{w:"crown",p:["p15"]},{w:"roots",p:["p15"]},{w:"stain",p:["p16"]},{w:"veneers",p:["p17"]},{w:"Glossary",p:["p18"]},{w:"Known",p:["p19"]},{w:"types",p:["p19"]},{w:"of",p:["p19","p21","p22"]},{w:"at",p:["p19","p20"]},{w:"glance",p:["p19","p20"]},{w:"most",p:["p20"]},{w:"popular",p:["p20"]},{w:"furnishing",p:["p20"]},{w:"styles",p:["p20"]},{w:"symbolism",p:["p21"]},{w:"trees",p:["p21"]},{w:"From",p:["p22"]},{w:"website",p:["p22"]},{w:"living",p:["p22"]},{w:"room,",p:["p22"]},{w:"how",p:["p22","p23","p28"]},{w:"does",p:["p22","p23"]},{w:"my",p:["p22"]},{w:"favorite",p:["p22"]},{w:"piece",p:["p22"]},{w:"get",p:["p22"]},{w:"home?",p:["p22"]},{w:"Complaints",p:["p23"]},{w:"it",p:["p23"]},{w:"work?",p:["p23"]},{w:"Removing",p:["p24","p25","p26"]},{w:"worms",p:["p24"]},{w:"scratches",p:["p25"]},{w:"stains",p:["p26"]},{w:"First",p:["p27"]},{w:"aid",p:["p27"]},{w:"tips",p:["p27"]},{w:"tricks",p:["p27"]},{w:"Order",p:["p28"]},{w:"procedure",p:["p28"]},{w:"buy",p:["p28"]},{w:"in",p:["p28"]},{w:"online",p:["p28"]},{w:"shop",p:["p28"]}];
 var ph={};
 ph["p0"]=[0, 1, 2, 3];
 ph["p1"]=[4, 5];
@@ -17,17 +17,18 @@ ph["p14"]=[42];
 ph["p13"]=[41];
 ph["p16"]=[5, 45];
 ph["p15"]=[43, 44];
-ph["p18"]=[47, 48, 49, 5, 50, 19, 51];
+ph["p18"]=[47];
 ph["p17"]=[46];
-ph["p19"]=[2, 52, 53, 54, 55, 50, 19, 51];
-ph["p21"]=[58, 2, 59, 8, 2, 60, 61, 62, 63, 64, 65, 66, 49, 10, 67, 8, 64, 68];
-ph["p20"]=[2, 56, 49, 57];
-ph["p23"]=[72, 5, 73];
-ph["p22"]=[69, 18, 62, 63, 70, 71];
-ph["p25"]=[72, 75];
-ph["p24"]=[72, 74];
-ph["p27"]=[80, 81, 18, 62, 8, 82, 83, 0, 84, 85];
-ph["p26"]=[76, 77, 78, 15, 79, 12, 4, 10];
+ph["p19"]=[48, 49, 50, 5, 51, 19, 52];
+ph["p21"]=[2, 57, 50, 58];
+ph["p20"]=[2, 53, 54, 55, 56, 51, 19, 52];
+ph["p23"]=[70, 18, 63, 64, 71, 72];
+ph["p22"]=[59, 2, 60, 8, 2, 61, 62, 63, 64, 65, 66, 67, 50, 10, 68, 8, 65, 69];
+ph["p25"]=[73, 75];
+ph["p24"]=[73, 5, 74];
+ph["p27"]=[77, 78, 79, 15, 80, 12, 4, 10];
+ph["p26"]=[73, 76];
+ph["p28"]=[81, 82, 18, 63, 8, 83, 84, 0, 85, 86];
      return {
          keywords: keywords,
          ph: ph
